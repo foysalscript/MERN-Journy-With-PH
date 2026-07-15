330d8c2d8c728f27860c5df0db2982a256321cb4
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-16 00:07:49 +0600

    feat(js-practice): Add personal bio

---
+++ Java Script/Javascript Practice Task/Task 06 - Personal Bio/app.js
@@ -0,0 +1,14 @@
+let fullName = "Foysal Hossien";
+let age = 24;
+let favoriteFood = "Kacchi";
+let favoriteProgrammingLanguage = "Javascript";
+let loverCoding = true;
+
+console.log("Hello");
+console.log(`My name is ${fullName}`);
+console.log(`I'm ${age} years old`);
+console.log(`My favorite food is ${favoriteFood}`);
+console.log(
+  `My favorite programming language is ${favoriteProgrammingLanguage}`,
+);
+console.log(`I loves coding ${loverCoding}`);
