e89ddbaeb55f5cf94072c7834a55392ad43f45e8
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-18 00:05:39 +0600

    feat(js): complete comparison operators practice

---
+++ Java Script/Javascript Practice Task/Task 10 - Comparison Operators/app.js
@@ -0,0 +1,31 @@
+const price1 = 25;
+const price2 = 20;
+
+if (price1 == price2) {
+  console.log("Value are same");
+}
+
+if (price1 === price2) {
+  console.log("Value and data type are same");
+}
+
+if (price1 != price2) {
+  console.log("Value are not same");
+}
+if (price1 !== price2) {
+  console.log("Value and data type are not same");
+}
+
+if (price1 > price2) {
+  console.log("25 is grater then 20");
+}
+if (price1 < price2) {
+  console.log("25 is less then 20");
+}
+
+if (price1 >= price2) {
+  console.log("25 is grater or equal then 20");
+}
+if (price1 <= price2) {
+  console.log("25 is less or equal then 20");
+}
