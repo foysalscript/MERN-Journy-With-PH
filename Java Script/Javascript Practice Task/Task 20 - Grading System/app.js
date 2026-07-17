6daf6ff5796c13324a51f6da6ceaa41302814fef
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-18 02:03:01 +0600

    feat(js): build grading system using if else if

---
+++ Java Script/Javascript Practice Task/Task 20 - Grading System/app.js
@@ -0,0 +1,17 @@
+const marks = 56;
+
+if (marks >= 80) {
+  console.log("A+");
+} else if (marks >= 70) {
+  console.log("A");
+} else if (marks >= 60) {
+  console.log("B");
+} else if (marks >= 50) {
+  console.log("C");
+} else if (marks >= 40) {
+  console.log("D");
+} else if (marks >= 33) {
+  console.log("E");
+} else {
+  console.log("F");
+}
